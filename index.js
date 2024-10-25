3b23a83357c5d62a65191c0894599e8f4981086d
1wag <69050009+1wag@users.noreply.github.com>
2024-10-25 17:48:47 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,27 +1,70 @@
-let currentIndex = 0;
-    const paragraphs = document.querySelectorAll('.paragraph');
-    const intervalTime = 5000;
+document.getElementById("instagram").addEventListener("click", function() {
+  window.open("https://instagram.com/autoskola_krystof_brno");
+});
 
-    function toggleMenu() {
-      const navMenu = document.getElementById('nav-menu');
-      navMenu.classList.toggle('hidden');
+document.getElementById("facebook").addEventListener("click", function() {
+  window.open("https://www.facebook.com/p/Auto%C5%A1kola-Kry%C5%A1tof-61563449555120/");
+});
+
+document.getElementById("ucebnice").addEventListener("click", function() {
+  window.open("https://www.schroter.cz/pristup/kr02jm/brno-krystof.pdf");
+});
+
+
+let currentParagraphIndex = 0;
+let totalParagraphs = document.querySelectorAll('.paragraph').length;
+let interval;
+
+function showParagraph(index) {
+    clearActiveStates();
+    currentParagraphIndex = index;
+
+    document.querySelectorAll('.paragraph')[index].classList.add('active');
+
+    document.getElementById(`btn-${index}`).classList.add('active');
+}
+
+function clearActiveStates() {
+
+    document.querySelectorAll('.paragraph').forEach(paragraph => {
+        paragraph.classList.remove('active');
+    });
+
+    document.querySelectorAll('.button-container button').forEach(button => {
+        button.classList.remove('active');
+    });
+}
+
+function startAutoCycle() {
+    interval = setInterval(() => {
+        currentParagraphIndex = (currentParagraphIndex + 1) % totalParagraphs;
+        showParagraph(currentParagraphIndex);
+    }, 5000);
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    showParagraph(0);  
+    startAutoCycle();  
+});
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
     }
+  }
 
-    function showParagraph(index) {
-            const paragraphs = document.querySelectorAll('.paragraph');
+  document.addEventListener("DOMContentLoaded", function() {
+   
+    var map = L.map('map').setView([49.18117612639365,16.620124411], 16);
 
-            paragraphs.forEach((p, i) => {
-                if (i === index) {
-                    p.classList.add('active');
-                } else {
-                    p.classList.remove('active');
-                }
-            });
-        }
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
 
-        function cycleParagraphs() {
-      currentIndex = (currentIndex + 1) % paragraphs.length;
-      showParagraph(currentIndex);
-    }
+    var marker = L.marker([49.18117612639365,16.620124411]).addTo(map);
 
-    setInterval(cycleParagraphs, intervalTime);
+    marker.bindPopup("<b>Autoškola Kryštof</b><br>Komárovská 666/3, 617 00 Brno-jih-Komárov").openPopup();
+  });
